3fcf4d66ee635edd77addfb0d86f4a536850961f
Brodie M <brodiebowenmorgan76@gmail.com>
2024-12-02 10:41:05 +1100

    Tabs template setup

---
+++ personal-trainer-admin-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'personal-trainer-admin-app',
+  webDir: 'www'
+};
+
+export default config;
