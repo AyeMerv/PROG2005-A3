db11adf66b1433ccea885dc76e655717e7e5dfd4
mossy-isme <mossycluney@gmail.com>
2024-12-02 21:05:24 +1100

    modified:   personal-trainer-admin-app/src/app/pages/home/home.component.html - updated text 	modified:   personal-trainer-admin-app/src/app/tab1/tab1.page.html - updated tab names to home *TO-DO* - update routing to rename to home 	modified:   personal-trainer-admin-app/src/app/tab1/tab1.page.ts - removed placeholder explorercontainer element 	modified:   personal-trainer-admin-app/src/app/tabs/tabs.page.html - updated tab names to home

--- personal-trainer-admin-app/src/app/tab1/tab1.page.ts
+++ personal-trainer-admin-app/src/app/tab1/tab1.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { HomeComponent } from '../pages/home/home.component';
 
 @Component({
