9c5431ab410d0a0113c8db62759de07efbe6b43b
mossy-isme <mossycluney@gmail.com>
2024-12-03 20:08:39 +1100

    modified:   personal-trainer-admin-app/src/app/pages/login/login.component.html - adjusted html to incorporate form usage 	modified:   personal-trainer-admin-app/src/app/pages/login/login.component.ts - created login function for api calls to the RESTful website 	new file:   personal-trainer-admin-app/src/app/tab4/tab4.page.html - new tab for login 	new file:   personal-trainer-admin-app/src/app/tab4/tab4.page.scss - new tab for login 	new file:   personal-trainer-admin-app/src/app/tab4/tab4.page.spec.ts - new tab for login 	new file:   personal-trainer-admin-app/src/app/tab4/tab4.page.ts - new tab for login 	modified:   personal-trainer-admin-app/src/app/tabs/tabs.page.html - adjusted page to add tab4 	modified:   personal-trainer-admin-app/src/app/tabs/tabs.page.ts - adjusted page to add tab4 	modified:   personal-trainer-admin-app/src/app/tabs/tabs.routes.ts - adjusted route to add tab4

--- personal-trainer-admin-app/src/app/pages/login/login.component.ts
+++ personal-trainer-admin-app/src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';  // IonicModule
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -9,5 +10,46 @@ import { IonicModule } from '@ionic/angular';  // IonicModule
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  constructor() {}
+  email: string = '';
+  password: string = '';
+  toastVisible: boolean = false;
+  toastMessage: string = '';
+
+  /**
+   * Constructor for the class.
+   * @param {HttpClient} http - An instance of the HttpClient service.
+   */
+  constructor(private http: HttpClient) { }
+
+  login() {
+    const apiUrl = "https://nehemia.it.scu.edu.au/personaltrainer/personaltrainer/login";
+    const loginData = {
+      email: this.email,
+      password: this.password,
+    };
+
+    this.http.post(apiUrl, loginData).subscribe(
+      (response: any) => {
+        if (response.success) {
+          this.toastMessage = 'Login successful!';
+        } else {
+          this.toastMessage = 'Invalid username or password.';
+        }
+        this.showToast();
+      },
+      (error) => {
+        console.error('API error:', error);
+        this.toastMessage = 'An error occurred. Please try again.';
+        this.showToast();
+      }
+    );
+  }
+
+  showToast() {
+    this.toastVisible = true;
+    setTimeout(() => {
+      this.toastVisible = false;
+    }, 2000);
+  }
+
 }
