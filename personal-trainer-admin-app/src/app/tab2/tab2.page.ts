2230818de0940f28f635542c556286ef5214cfc5
Brodie M <brodiebowenmorgan76@gmail.com>
2024-12-04 14:48:57 +1100

    Styling Tabs

--- personal-trainer-admin-app/src/app/tab2/tab2.page.ts
+++ personal-trainer-admin-app/src/app/tab2/tab2.page.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { ClientsListComponent } from '../pages/clients-list/clients-list.component';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ClientsListComponent],
 })
 export class Tab2Page {
-
   constructor() {}
-
 }
