d5a4e52c59e89baff7bda8cfb460c98daf2766cf
mossy-isme <mossycluney@gmail.com>
2024-12-08 10:44:20 +1100

    Created home component for client-app

---
+++ client-app/src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [IonicModule], 
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  trainerName=""
+  constructor() {}
+}
