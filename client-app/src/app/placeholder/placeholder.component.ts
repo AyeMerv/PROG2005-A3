1af8cd90aa3ff0b976abedfa49a270da2b818611
mossy-isme <mossycluney@gmail.com>
2024-12-08 10:54:06 +1100

    created placeholder component

---
+++ client-app/src/app/placeholder/placeholder.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-placeholder',
+  standalone: true,
+
+  templateUrl: './placeholder.component.html',
+  styleUrls: ['./placeholder.component.scss'],
+})
+export class PlaceholderComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
