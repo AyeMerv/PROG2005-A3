b4e5a7d4166ba3d018ea1b5a946abc8e8b036565
mossy-isme <mossycluney@gmail.com>
2024-12-08 18:09:13 +1100

    Add client workout page and functionality, and routing

--- client-app/src/app/pages/client-workout/client-workout.component.ts
+++ client-app/src/app/pages/client-workout/client-workout.component.ts
@@ -1,14 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import { NgFor, NgIf } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-client-workout',
+  standalone: true,
   templateUrl: './client-workout.component.html',
   styleUrls: ['./client-workout.component.scss'],
+  imports: [IonicModule, NgFor, NgIf],
 })
-export class ClientWorkoutComponent  implements OnInit {
+export class ClientWorkoutComponent implements OnInit {
+  clientId: number | null = null; 
+  workoutPlan: any[] | null = null;  
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService,
+    private authService: AuthService,
+  ) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.clientId = this.authService.getClientId();
+    if (this.clientId !== null) {
+      this.apiService.getWorkoutPlan(this.clientId).subscribe({
+        next: (data) => {
+          console.log(data)
+          this.workoutPlan = data;
+        },
+        error: (err) => console.error('Error fetching workout plan:', err),
+      });
+    } else {
+      console.error('Client ID not found!');
+    }
+  }
 
 }
--- client-app/src/app/tab3/tab3.page.ts
+++ client-app/src/app/tab3/tab3.page.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { PlaceholderComponent } from '../placeholder/placeholder.component';
+import { ClientWorkoutComponent } from "../pages/client-workout/client-workout.component";
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, PlaceholderComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ClientWorkoutComponent],
 })
 export class Tab3Page {
   constructor() {}
