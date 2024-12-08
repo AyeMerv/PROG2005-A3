29484d24a5b8ea31ab655993edb7159436e2c0cd
mossy-isme <mossycluney@gmail.com>
2024-12-08 19:26:26 +1100

    Created privacy page

--- client-app/src/app/pages/privacy/privacy.component.ts
+++ client-app/src/app/pages/privacy/privacy.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-privacy',
+  standalone: true,
   templateUrl: './privacy.component.html',
   styleUrls: ['./privacy.component.scss'],
+  imports: [IonicModule]
 })
-export class PrivacyComponent  implements OnInit {
+export class PrivacyComponent {
 
   constructor() { }
-
-  ngOnInit() {}
 
 }
